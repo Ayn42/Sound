90b6bbef356a8f6ac5bf519cd13d58701daf9b31
Ayanon Fukui <cainaifujing49@gmail.com>
2020-05-11 13:07:51 +0900

    Sound2のプロジェクトを追加した

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    //ドラムを表示する箱を作る
+    @IBOutlet var drumButton: UIButton!
     //ドラムのサウンドファイルを読み込んでプレイヤーを作る
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
     
@@ -17,11 +20,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    //ドラムがタップされた時に呼ばれる
-    @IBAction func tapDumButton(){
+    //ドラムがタッチされた時に呼ばれる
+    @IBAction func touchDownDumButton(){
+        //ドラムが鳴っている画像に切り替える
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         //ドラムの音を巻き戻す
         drumSoundPlayer.currentTime = 0
         //ドラムの音を再生する
         drumSoundPlayer.play()
+    }
+    //ドラムボタンの上でタッチが終わる時に呼ばれる
+    @IBAction func touchUpDrumButton(){
+        //ドラムがなっていない画像に切り替える
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
     }
 }
