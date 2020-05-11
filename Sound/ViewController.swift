dc2eddd3d9d26edf566ab21a93dd393794eb1b74
Ayanon Fukui <cainaifujing49@gmail.com>
2020-05-11 13:48:20 +0900

    ピアノとギターとシンバルの３つも追加し合計４つの演奏にした

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -13,25 +13,59 @@ class ViewController: UIViewController {
     
     //ドラムを表示する箱を作る
     @IBOutlet var drumButton: UIButton!
+    //ピアノを表示する箱を作る
+    @IBOutlet var pianoButton: UIButton!
+    @IBOutlet var cymbalButton: UIButton!
+    @IBOutlet var guitarButton: UIButton!
+    
     //ドラムのサウンドファイルを読み込んでプレイヤーを作る
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    //ピアノのサウンドファイルを読み込んでプレイヤーを作る
+    let pianoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name:"pianoSound")!.data)
+    
+    let guitarSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name:"guitarSound")!.data)
+    
+    let cymbalSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name:"cymbalSound")!.data)
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        super.viewDidLoad()}
+        // Do any additional setup after loading the view
+    
     //ドラムがタッチされた時に呼ばれる
-    @IBAction func touchDownDumButton(){
+    @IBAction func touchDownDrumButton(){
         //ドラムが鳴っている画像に切り替える
         drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         //ドラムの音を巻き戻す
         drumSoundPlayer.currentTime = 0
         //ドラムの音を再生する
-        drumSoundPlayer.play()
-    }
+        drumSoundPlayer.play()}
+    
+    @IBAction func touchDownPianoButton(){
+    //ピアノが鳴っている画像に切り替える
+               pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+               //ピアノの音を巻き戻す
+               pianoSoundPlayer.currentTime = 0
+               //ピアノの音を再生する
+               pianoSoundPlayer.play()}
+    
+    @IBAction func touchDownGuitarButton(){
+                  guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+                  guitarSoundPlayer.currentTime = 0
+                  guitarSoundPlayer.play()}
+    
+    @IBAction func touchDownCymbalButton(){
+                  cymbalButton.setImage(UIImage(named: "cymbalPlayingImage"), for: .normal)
+                  cymbalSoundPlayer.currentTime = 0
+                  cymbalSoundPlayer.play()}
+    
     //ドラムボタンの上でタッチが終わる時に呼ばれる
     @IBAction func touchUpDrumButton(){
         //ドラムがなっていない画像に切り替える
-        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
-    }
+            drumButton.setImage(UIImage(named: "drumImage"), for: .normal)}
+    @IBAction func touchUpPianoButton(){
+            pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)}
+    @IBAction func touchUpGuitarButton(){
+             guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)}
+    @IBAction func touchUpCymbalButton(){
+             cymbalButton.setImage(UIImage(named: "cymbalImage"), for: .normal)}
 }
